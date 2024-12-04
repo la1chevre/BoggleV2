227f6863021cc87d065d1aa377894cc06fa7f937
SioracLeVrai <103414597+SioracLeVrai@users.noreply.github.com>
2024-12-04 11:32:02 +0100

    Program.cs

--- BoggleV2/Program.cs
+++ BoggleV2/Program.cs
@@ -1,24 +1,47 @@
-﻿namespace BoggleV2
+namespace BoggleV2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("test");
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("|                         BOGGLE                         |");
+            Console.WriteLine("+--------------------------------------------------------+");
+            Console.WriteLine("\npar Grégoire LEGROS et Maximilien BERNARD");
 
-            Dictionnaire dico = new Dictionnaire("fr");
-            for (int i = 0; i < dico.dictionnaire.Length; i++)
-            {
-                Console.WriteLine(dico.dictionnaire[i]);
+            Console.WriteLine("Dans quelle langue voulez vous jouer ?");
+            Dictionnaire dico = CreationMots();
 
-            }
             Plateau plato = new Plateau(4);
             Console.WriteLine(plato.toString());
             string mot = Console.ReadLine();
             Console.WriteLine(mot);
             Console.WriteLine(plato.Test_Plateau(mot, dico));
             Console.WriteLine("azr");
+        }
+
+        static Dictionnaire CreationMots()
+        {
+            Console.WriteLine("\n\tTaper \"fr\" pour jouer en français et \"en\" pour jouer en anglais.");
+
+            string langue = Console.ReadLine();
+
+            switch (langue)
+            {
+                case "fr":
+                    return new Dictionnaire("fr");
+                    break;
+                case "en";
+                    return new Dictionnaire("en");
+                    break;
+                default:
+                    Console.WriteLine("Cette langue n'est pas reconnue.");
+                    return CreationMots();
+                    break;
+            }
+
+
+
         }
     }
 }
